a0ff9b4963ea32ca08c81a1c340c86b9eab11d43
DeveloperThiagoRodrigues <onlinedigitalnegocios@gmail.com>
2026-02-20 20:17:21 -0300

    Implement static file serving for uploads
    Add static file serving for product and category uploads.

--- src/config/multer.cjs
+++ src/config/multer.cjs
@@ -3,8 +3,15 @@ const { resolve } = require('node:path');
 const { v4 } = require('uuid');
 
 module.exports = {
-    storage: multer.diskStorage({
-        destination: resolve(__dirname, '..', '..', 'uploads'),
+    app.use(
+  '/product-file',
+  express.static(path.resolve(__dirname, '..', 'uploads/products'))
+);
+
+app.use(
+  '/category-file',
+  express.static(path.resolve(__dirname, '..', 'uploads/categories'))
+);
         filename: (_request, file, callback) => {
             const uniqueName = v4().concat(`-${file.originalname}`);
             return callback(null, uniqueName);
