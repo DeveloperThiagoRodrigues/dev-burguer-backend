fa882036c5d4bc5788979263fff2d6f71fcdaf36
DeveloperThiagoRodrigues <onlinedigitalnegocios@gmail.com>
2026-02-20 18:06:54 -0300

    Update Category.js

--- src/app/models/Category.js
+++ src/app/models/Category.js
@@ -9,7 +9,7 @@ class Category extends Model {
         url: {
           type: Sequelize.VIRTUAL,
           get() {
-            return `${process.env.APP_URL}/categoryfile/${this.path}`;
+            return `http://localhost:3001/product-file/${this.path}`;
           }
         }
       },
