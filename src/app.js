2d91d17606d1677ee85b69ce565ad38f12c1db15
DeveloperThiagoRodrigues <onlinedigitalnegocios@gmail.com>
2026-02-20 18:52:31 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,9 +2,7 @@ import express from 'express';
 import routes from './routes.js';
 import fileRouteConfig from './config/fileRoutes.cjs';
 import cors from 'cors';
-
-
-
+import 'dotenv/config';
 
 const app = express();
 
