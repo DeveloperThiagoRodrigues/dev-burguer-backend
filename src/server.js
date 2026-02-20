d4ce7ee5544e4e4eca7050605de9186417d2e542
DeveloperThiagoRodrigues <onlinedigitalnegocios@gmail.com>
2026-02-20 20:10:49 -0300

    Set default port to 3001 in server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import app from './app.js'
 import './database/index.js'
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3001;
 
 
 app.listen(3001, () => console.log("Server is running at port 3001..."));
