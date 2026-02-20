9d5374d5785140d98720dc9e3c19a25772a02591
DeveloperThiagoRodrigues <onlinedigitalnegocios@gmail.com>
2026-02-20 19:24:30 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,12 +6,12 @@ import 'dotenv/config';
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
   origin: process.env.CORS_ORIGIN,
 }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/product-file', fileRouteConfig);
 app.use('/category-file', fileRouteConfig);
