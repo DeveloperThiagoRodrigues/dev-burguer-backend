29bb51fe6e56b71e76fb0621516e030ee72c7eea
DeveloperThiagoRodrigues <onlinedigitalnegocios@gmail.com>
2026-02-21 11:53:27 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,21 +3,21 @@ import routes from './routes.js';
 import fileRouteConfig from './config/fileRoutes.cjs';
 import cors from 'cors';
 
+
+const app = express();
 const corsOptions = {
     origin: process.env.CORS_ORIGIN, // Puxa o valor do Easypanel
     credentials: true, // Importante para o login
     optionsSuccessStatus: 200
 };
-const app = express();
+
+app.use(cors(corsOptions)); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
-
 app.use('/product-file', fileRouteConfig);
 app.use('/category-file', fileRouteConfig);
 
 
-app.use(cors(corsOptions));
+app.use(routes);
 
 export default app;
