f4d9ac1a5d3d6bd8081279f0daa67d0d11ebde70
DeveloperThiagoRodrigues <onlinedigitalnegocios@gmail.com>
2026-02-21 17:03:51 -0300

    Update database.cjs

--- src/config/database.cjs
+++ src/config/database.cjs
@@ -1,15 +1,14 @@
-
-
 module.exports = {
   dialect: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  username: process.env.DB_USER, // Precisa ser igual ao nome no Easypanel
-  password: process.env.DB_PASS, // Precisa ser igual ao nome no Easypanel
-  database: process.env.DB_NAME, // Precisa ser igual ao nome no Easypanel
+  host: process.env.DB_HOST || 'db_postgres',
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   port: process.env.DB_PORT || 5432,
   define: {
     timestamps: true,
     underscored: true,
     underscoredAll: true,
   },
+};
 
