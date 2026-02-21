46defae67c5fa068dcf797601381aaf9cba9f411
DeveloperThiagoRodrigues <onlinedigitalnegocios@gmail.com>
2026-02-21 09:47:37 -0300

    Update multer.cjs

--- src/config/multer.cjs
+++ src/config/multer.cjs
@@ -1,10 +1,10 @@
 const multer = require('multer');
-const path = require('path');
+const { resolve } = require('node:path');
 const { v4 } = require('uuid');
 
 module.exports = {
     storage: multer.diskStorage({
-        destination: path.resolve(process.cwd(), 'uploads'), // ← usa process.cwd() = /app
+        destination: resolve(__dirname, '..', '..', 'uploads'),
         filename: (_request, file, callback) => {
             const uniqueName = v4().concat(`-${file.originalname}`);
             return callback(null, uniqueName);
