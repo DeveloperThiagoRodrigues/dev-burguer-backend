516b2f076f0901c619cb2e86047b7503af77c731
DeveloperThiagoRodrigues <onlinedigitalnegocios@gmail.com>
2026-02-21 09:16:09 -0300

    Remove unused import for OrderController

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,6 @@ import UserController from './app/controllers/UserController.js';
 import multerConfig from './config/multer.cjs';
 import authMiddleware from './app/middlewares/auth.js';
 import CategoryController from './app/controllers/CategoryController.js';
-
 import OrderController from './app/controllers/OrderController.js';
 import CreatePaymentIntentController from './app/controllers/stripe/CreatePaymentIntentController.js';
 
