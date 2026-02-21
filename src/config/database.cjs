a0c24430beb07dc21cb414d8f0d82d003104516f
DeveloperThiagoRodrigues <onlinedigitalnegocios@gmail.com>
2026-02-21 12:17:28 -0300

    Update database.cjs

--- src/config/database.cjs
+++ src/config/database.cjs
@@ -1,15 +1,12 @@
 module.exports = {
   dialect: 'postgres',
-  // Aqui está a mudança principal: ele tenta ler a variável do Easypanel, 
-  // e se não existir, usa 'localhost' como padrão para o seu PC
   host: process.env.DB_HOST || 'localhost',
-  username: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASS || 'postgres',
-  database: process.env.DB_NAME || 'devburger',
+  username: process.env.DB_USER, // Precisa ser igual ao nome no Easypanel
+  password: process.env.DB_PASS, // Precisa ser igual ao nome no Easypanel
+  database: process.env.DB_NAME, // Precisa ser igual ao nome no Easypanel
   port: process.env.DB_PORT || 5432,
   define: {
     timestamps: true,
     underscored: true,
     underscoredAll: true,
   },
-};
