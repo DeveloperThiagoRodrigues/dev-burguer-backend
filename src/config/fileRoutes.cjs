b7821f09e2f1c92a3cb0df79f29a6555d5c48146
DeveloperThiagoRodrigues <onlinedigitalnegocios@gmail.com>
2026-02-21 09:47:24 -0300

    Update fileRoutes.cjs

--- src/config/fileRoutes.cjs
+++ src/config/fileRoutes.cjs
@@ -1,7 +1,7 @@
 const { resolve } = require('node:path');
 const express = require('express');
 
-const uploadPath = resolve(process.cwd(), 'uploads');
+const uploadPath = resolve(__dirname, '..', '..', 'uploads');
 
 const fileRouteConfig = express.static(uploadPath);
 
