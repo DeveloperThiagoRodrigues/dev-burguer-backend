f4f7866b58c448fdfd5d27308e8749e426110651
DeveloperThiagoRodrigues <onlinedigitalnegocios@gmail.com>
2026-02-21 09:48:33 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 import app from './app.js'
 import './database/index.js'
 
-const PORT = process.env.PORT || 3001;
+const port = process.env.PORT || 3001
 
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+
+
+
+app.listen(3001, () => console.log("Server is running at port 3001..."));
 
