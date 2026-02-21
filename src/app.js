2bb9d67d8a329250c7bc620708bc347c0e32ef0e
DeveloperThiagoRodrigues <onlinedigitalnegocios@gmail.com>
2026-02-21 11:22:40 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,13 +5,14 @@ import cors from 'cors';
 
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
   origin: process.env.CORS_ORIGIN,
 }),
 );
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
 app.use('/product-file', fileRouteConfig);
 app.use('/category-file', fileRouteConfig);
 
