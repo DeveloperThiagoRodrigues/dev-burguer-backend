a0a40cc475d46b7c697c13194e7fa3e9668230a6
DeveloperThiagoRodrigues <onlinedigitalnegocios@gmail.com>
2026-02-22 16:40:39 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
+import './database/index.js'; // ✅ ADICIONE ESSA LINHA
 import app from './app.js';
 
 const PORT = process.env.PORT || 3001;
-
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running at port ${PORT}`);
 });
