eb139e5d126f1d0c19037b2b0475984ca3df7a58
DeveloperThiagoRodrigues <onlinedigitalnegocios@gmail.com>
2026-02-22 14:28:08 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 import express from "express";
 
-const app = express();   // ← ISSO ESTÁ FALTANDO
+const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 app.get("/", (req, res) => {
-  res.send("API rodando 🚀");
+  res.send("API funcionando 🚀");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
