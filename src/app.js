2ec8f9065ebc0004af8c50bbdabdc74a605c8e17
DeveloperThiagoRodrigues <onlinedigitalnegocios@gmail.com>
2026-02-22 15:47:47 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,12 +13,7 @@ const allowedOrigins = [
 app.use(cors({
   origin: allowedOrigins,
   credentials: true,
-  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type','Authorization']
 }));
-
-// ESSENCIAL PARA PREFLIGHT
-app.options('*', cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
