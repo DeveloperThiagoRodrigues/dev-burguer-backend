ac4cbeaa24a7bb9bc6d922eb272f597e9edc865b
DeveloperThiagoRodrigues <onlinedigitalnegocios@gmail.com>
2026-02-22 16:24:08 -0300

    Update UserController.js

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -6,51 +6,46 @@ import bcrypt from 'bcrypt';
 
 class UserController {
     async store(request, response) {
-        const schema = Yup.object({
-            name: Yup.string().required(),
-            email: Yup.string().email().required(),
-            password: Yup.string().min(6).required(),
-            admin: Yup.boolean(),
-        })
+    const schema = Yup.object({
+        name: Yup.string().required(),
+        email: Yup.string().email().required(),
+        password: Yup.string().min(6).required(),
+        admin: Yup.boolean(),
+    });
 
-        try {
-          schema.validateSync(request.body, { abortEarly: false }); 
-        } catch (err) {
-            return response.status(400).json({ error: err.errors});
-            
-        }
-        
-
-
-        const { name, email, password, admin = false } = request.body;
-
-        const existingUser = await User.findOne({
-            where: { 
-                email 
-            }
-        });
-
-        if (existingUser){
-            return response.status(400).json({ message: 'Este e-mail já está cadastado!'})
-        }
-
-        const password_hash = await bcrypt.hash(password, 10); 
-
-        const user = await User.create({
-            id: v4(),
-            name,
-            email,
-            password_hash,
-            admin,
-        });
-
-        response.status(201).json({
-            id: user.id,
-            name: user.name,
-            email: user.email,
-            admin: user.admin,
-        })
+    try {
+        await schema.validate(request.body, { abortEarly: false });
+    } catch (err) {
+        return response.status(400).json({ error: err.errors });
     }
+
+    const { name, email, password, admin = false } = request.body;
+
+    const existingUser = await User.findOne({
+        where: { email }
+    });
+
+    if (existingUser) {
+        return response.status(400).json({ message: 'Este e-mail já está cadastrado!' });
+    }
+
+    const password_hash = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+        id: v4(),
+        name,
+        email,
+        password_hash,
+        admin,
+    });
+
+    return response.status(201).json({
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        admin: user.admin,
+    });
+}
 }
 
 export default new UserController();
