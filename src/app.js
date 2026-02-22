d2c1540a4114e3f7cf3f8e4171b554d981ebd5ab
DeveloperThiagoRodrigues <onlinedigitalnegocios@gmail.com>
2026-02-22 15:43:42 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import routes from './routes.js';
 import fileRouteConfig from './config/fileRoutes.cjs';
 import cors from 'cors';
 
+const app = express();
+
 const allowedOrigins = [
   'http://localhost:5173',
   'https://dev-burguer-devburger-frontend.1e7gn0.easypanel.host'
@@ -17,6 +19,12 @@ app.use(cors({
 
 // ESSENCIAL PARA PREFLIGHT
 app.options('*', cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/product-file', fileRouteConfig);
+app.use('/category-file', fileRouteConfig);
 
 app.use(routes);
 
