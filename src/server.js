3a0d8dee914317dd184d7f046a0780ccd4f830e8
DeveloperThiagoRodrigues <onlinedigitalnegocios@gmail.com>
2026-02-22 15:45:49 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,13 +1,7 @@
-import express from "express";
-
-const app = express();
+import app from './app.js';
 
 const PORT = process.env.PORT || 3001;
 
-app.get("/", (req, res) => {
-  res.send("API funcionando 🚀");
-});
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server is running at port ${PORT}...`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running at port ${PORT}`);
 });
