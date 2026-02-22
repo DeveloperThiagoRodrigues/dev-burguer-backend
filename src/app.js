bcd13cf91c2289b45cbbb962cfc22c977a8f9936
DeveloperThiagoRodrigues <onlinedigitalnegocios@gmail.com>
2026-02-22 15:33:00 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,16 +3,27 @@ import routes from './routes.js';
 import fileRouteConfig from './config/fileRoutes.cjs';
 import cors from 'cors';
 
-
 const app = express();
 
-app.use(cors());
+// ✅ libera frontend do easypanel e localhost
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'https://dev-burguer-devburger-frontend.1e7gn0.easypanel.host'
+  ],
+  methods: ['GET','POST','PUT','DELETE','PATCH'],
+  allowedHeaders: ['Content-Type','Authorization']
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// arquivos estáticos
 app.use('/product-file', fileRouteConfig);
 app.use('/category-file', fileRouteConfig);
 
-
+// rotas da API
 app.use(routes);
 
 export default app;
