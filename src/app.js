4a0608b862ab89082417aa727db400e7845d904b
DeveloperThiagoRodrigues <onlinedigitalnegocios@gmail.com>
2026-02-22 15:42:44 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,33 +3,20 @@ import routes from './routes.js';
 import fileRouteConfig from './config/fileRoutes.cjs';
 import cors from 'cors';
 
-const app = express();
-
 const allowedOrigins = [
   'http://localhost:5173',
-  'http://localhost:3000',
   'https://dev-burguer-devburger-frontend.1e7gn0.easypanel.host'
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    // permite requisição sem origin (postman, mobile, etc)
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(null, true); // libera tudo (produção simples)
-    }
-  },
-  credentials: true
+  origin: allowedOrigins,
+  credentials: true,
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type','Authorization']
 }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use('/product-file', fileRouteConfig);
-app.use('/category-file', fileRouteConfig);
+// ESSENCIAL PARA PREFLIGHT
+app.options('*', cors());
 
 app.use(routes);
 
